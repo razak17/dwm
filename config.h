7e7cfdf08592d9bbfc7a1a8215a451c593de3bf9
Razak Mo <moabdulrazak17@gmail.com>
2024-07-19 15:47:30 +0000

    feat: use my colors

--- config.h
+++ config.h
@@ -10,17 +10,9 @@ static const char *fonts[]    = {
     "Noto Color Emoji:pixelsize=11:antialias=true:autohint=true",
     "JoyPixels:pixelsize=9:antialias=true:autohint=true"
 };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-};
+static const char dmenufont[] = "Operator Mono Lig Book:size=9";
+
+#include "/home/razak/.cache/wal/colors-wal-dwm.h"
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
