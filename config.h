3c84f14efbc6090c0fa044dfdd49fa364587ff65
Abdul Razak <moabdulrazak17@gmail.com>
2024-10-27 15:38:13 +0000

    feat: update tagging key

--- config.h
+++ config.h
@@ -119,7 +119,7 @@ static const Layout layouts[] = {
 #define TAGKEYS(KEY,TAG)                                                        \
     { MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
     { ControlMask|ALTMOD,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-    { ALTMOD,                       KEY,      tag,            {.ui = 1 << TAG} }, \
+    { MODKEY | ShiftMask,           KEY,      tag,            {.ui = 1 << TAG} }, \
     { MODKEY|ALTMOD,                KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
